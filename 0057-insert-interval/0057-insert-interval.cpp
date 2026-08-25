72b2aec93d42398b541e94a34d6ec6d70c16f9ab
Arpit Srivastav <arpitsrivastav7839@gmail.com>
2026-08-25 11:20:12 +0530

    Time: 0 ms (100%), Space: 21.7 MB (76.61%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res;
-        int start = newInterval[0], end = newInterval[1];
+        vector<vector<int>>res;
+        int start = newInterval[0];
+        int end = newInterval[1];
         bool inserted = false;
 
         for(auto &interval : intervals){
-            if(interval[1] < start){
-                // current interval ends before newInterval starts
+            if(interval[1]<start){//current element end ho raha jaha se newInterval start ho raha 
                 res.push_back(interval);
             }
-            else if(interval[0] > end){
-                // current interval starts after newInterval ends
-                if(!inserted){
-                    res.push_back({start,end});
+            else if(interval[0] > end){//current interval start ho raha jab new Interval end ho raha
+                if(!inserted) {
+                    res.push_back({start,end});//isiliye newinterval yaha insert hua 
                     inserted = true;
                 }
                 res.push_back(interval);
             }
             else{
-                // overlap → merge
-                start = min(start, interval[0]);
+                start = min(start , interval[0]);
                 end = max(end, interval[1]);
             }
-        }
 
-        if(!inserted){
-            res.push_back({start,end});
         }
-
+        if(!inserted) res.push_back({start,end});
         return res;
     }
 };
